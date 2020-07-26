efe902df482375f067a7d5fe643bb5dc9fcb8a1c
GaryGosh <clichethe1@yahoo.com>
2020-07-26 13:29:52 +0530

    Updates

---
+++ precache-manifest.f682bb7e13e27a645fd0150ca43e9692.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266237ca29c44e1e3e05918d5c39f3ac",
+    "url": "/Covid-19-Tracker-Web/index.html"
+  },
+  {
+    "revision": "d0cc7dccd5e1f48e60c0",
+    "url": "/Covid-19-Tracker-Web/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "fe00488f478c5d3f25ed",
+    "url": "/Covid-19-Tracker-Web/static/css/main.202b55c3.chunk.css"
+  },
+  {
+    "revision": "d0cc7dccd5e1f48e60c0",
+    "url": "/Covid-19-Tracker-Web/static/js/2.a7764eb6.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid-19-Tracker-Web/static/js/2.a7764eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe00488f478c5d3f25ed",
+    "url": "/Covid-19-Tracker-Web/static/js/main.5c1aec3b.chunk.js"
+  },
+  {
+    "revision": "8712fc06572adc8de650",
+    "url": "/Covid-19-Tracker-Web/static/js/runtime-main.ad83c2f8.js"
+  }
+]);
